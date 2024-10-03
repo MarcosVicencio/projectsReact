d90deebd4999aaf731b3730a81f8a4a9d62a8848
MARCOS REGINALDO VICENCIO GARCIA <216673706>
2024-10-02 19:59:28 -0600

    021024

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+export function App (){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" alt="Avatar de React JS" />
+                <div>
+                    <strong>CSS Style Sheet</strong>
+                    <span>@CSS</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
